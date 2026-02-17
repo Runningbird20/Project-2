3a05417b115ee4c27a0e35181a0837b794b7983d
Runningbird20 <153043342+Runningbird20@users.noreply.github.com>
2026-02-17 17:39:23 -0500

    comfetti

--- apply/urls.py
+++ apply/urls.py
@@ -5,6 +5,7 @@ app_name = 'apply'
 
 urlpatterns = [
     path('submit/<int:job_id>/', views.submit_application, name='submit_application'),
+    path('submitted/<int:job_id>/', views.application_submitted, name='application_submitted'),
     path('status/', views.application_status, name='application_status'),
     path('update-status/<int:application_id>/', views.update_status, name='update_status'),
     path('pipeline/<int:job_id>/', views.employer_pipeline, name='employer_pipeline'),
