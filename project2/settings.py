197918f09391fbe8fc9c9b240464ae4a1b6bc6e1
Runningbird20 <153043342+Runningbird20@users.noreply.github.com>
2026-02-23 17:48:21 -0500

    UI fixes

--- project2/settings.py
+++ project2/settings.py
@@ -53,7 +53,6 @@ def _env(key, default=""):
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
