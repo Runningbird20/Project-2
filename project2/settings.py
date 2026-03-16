836b8f3a5e8dcfe238e95e6c726f18e1aa55fefe
Runningbird20 <153043342+Runningbird20@users.noreply.github.com>
2026-03-16 12:48:14 -0400

    vercel

--- project2/settings.py
+++ project2/settings.py
@@ -58,7 +58,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = _csv_env_list(
     "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
+    "127.0.0.1,localhost",
+    "pandapulse.vercel.app",
 )
 
 CSRF_TRUSTED_ORIGINS = _csv_env_list(
