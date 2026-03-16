d237ee1f059226a4e9a7c25e89cf759ab4e25e1e
Runningbird20 <153043342+Runningbird20@users.noreply.github.com>
2026-03-16 15:42:58 -0400

    host

--- project2/settings.py
+++ project2/settings.py
@@ -277,7 +277,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = (BASE_DIR / "public" / "static") if IS_VERCEL else (BASE_DIR / "staticfiles")
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
