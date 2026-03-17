650b841ccd923f3eee1eb49cd01ed7257d6c17a3
Jase George <161396394+AzureRaven25@users.noreply.github.com>
2026-03-16 22:55:08 -0400

    Update urls.py

--- pulses/urls.py
+++ pulses/urls.py
@@ -6,4 +6,5 @@ app_name = "pulses"
 urlpatterns = [
     path("", views.pulses_feed, name="feed"),
     path("upload/", views.upload_pulse, name="upload"),
+    path('delete/<int:pulse_id>/', views.delete_pulse, name='delete')
 ]
